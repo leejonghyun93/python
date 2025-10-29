6618e7cd1aa3643b5dd872861f2f6737e2767a5e
leejonghyun93 <fhohffodf93@gmail.com>
2025-10-29 12:49:10 +0900

    객체를 만드는 함수(1)

---
+++ object_2_dict.py
@@ -0,0 +1,26 @@
+def create_student(name, korean, math, english , science):
+    return{
+        "name":name,
+        "korean":korean,
+        "math":math,
+        "english":english,
+        "science":science
+    }
+
+students = [
+    create_student("홍길동",87,98,88,95),
+    create_student("김길동",87,98,88,95),
+    create_student("이길동",87,98,88,95),
+    create_student("박길동",87,98,88,95),
+    create_student("장길동",87,98,88,95),
+    create_student("김길동",87,98,88,95),
+]
+
+print("이름","총점","평균", sep="\t")
+
+for student in students:
+    score_sum = student["korean"] + student["math"] +\
+    student["english"]+ student["science"]
+    score_average = score_sum / 4
+
+    print(student["name"], score_sum,score_average, sep="\t")
